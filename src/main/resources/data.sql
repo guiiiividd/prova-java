dec75f3419a8048e43a80d4101cbad787bcb9fbc
guiiiividd <guiiiividd@gmail.com>
2024-06-25 08:54:57 -0300

    Commit 3 - Criação do Repositório e Script SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_CITY(nome, estado, populacao, pib) VALUES ('Itu', 'São Paulo', 200000, 1500000)
+INSERT INTO TBL_CITY(nome, estado, populacao, pib) VALUES ('Salto', 'São Paulo', 100000, 700000)
+INSERT INTO TBL_CITY(nome, estado, populacao, pib) VALUES ('Indaiatuba', 'São Paulo', 300000, 2500000)
