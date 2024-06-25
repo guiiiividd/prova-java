d113d418cab900fc062e154d1e6931748451ffa7
guiiiividd <guiiiividd@gmail.com>
2024-06-25 09:30:45 -0300

    Commit 6 - Implementando o end-point GET /cities/{id}

--- src/main/java/com/fatec/city/resources/CityController.java
+++ src/main/java/com/fatec/city/resources/CityController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CityController {
     @GetMapping
     public List<City> getCities(){
         return this.cityService.getCities();
+    }
+
+    @GetMapping("{id}")
+    public City getCityById(@PathVariable int id){
+        return this.cityService.getCityById(id);
     }
 }
--- src/main/java/com/fatec/city/services/CityService.java
+++ src/main/java/com/fatec/city/services/CityService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.fatec.city.entities.City;
 import com.fatec.city.repositories.CityRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CityService {
 
@@ -16,5 +18,11 @@ public class CityService {
     
     public List<City> getCities(){
         return cityRepository.findAll();
+    }
+
+    public City getCityById(int id){
+        return cityRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Cidade não cadastrada!")
+        );
     }
 }
